79975b158088a942c174123ca69a9604af85fc34
Isaac <isaac.wills17@gmail.com>
2022-09-14 16:33:56 -0700

    jdh

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,6 +2,7 @@
 
 /**
  * main - causes an infinite loop
+ *
  * Return: 0
  */
 
@@ -13,16 +14,12 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10)
-	{
-		putchar(i);
-	}
-	/**
-	 * while (i < 10)
-	 * {
-	 * 	putchar(i);
+	/*
+ 	 * while (i < 10)
+ 	 * {
+ 	*	putchar(i);
 	 * }
-	 */
+ 	 */
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
