7b1f8f354657a55e0cce6848d07216c8d20a3854
Isaac <isaac.wills17@gmail.com>
2022-09-30 08:29:01 -0700

    progress

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 /**
- * main - adds positive numbers.
- * @argc: number of command line arguments.
- * @argv: array that contains the program command line arguments.
- * Return: 0 - success.
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector for values
+ * Return: always 0
  */
+
 int main(int argc, char *argv[])
 {
-	int i, j, add = 0;
-	for (i = 1; i < argc; i++)
+	int i, j, sum;
+
+	sum = 0;
+
+	for (i = 1; i < argc ; i++)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		for (j = 0; argv[i][j] != '\0' ; j++)
 		{
-			if (!isdigit(argv[i][j]))
+			if (argv[i][j] < 47 || argv[i][j] > 57)
 			{
 				printf("Error\n");
 				return (1);
 			}
 		}
-		add += atoi(argv[i]);
+		sum = sum + atoi(argv[i]);
 	}
-	printf("%d\n", add);
+	printf("%d\n", sum);
 	return (0);
 }
